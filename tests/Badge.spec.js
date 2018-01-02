4d4827449c7f9cf88088302ad2e28e842f22b811
飞酱 <zjf111435@alibaba-inc.com>
2018-01-02 20:06:01 +0800

    test: improve coverage

--- tests/Badge.spec.js
+++ tests/Badge.spec.js
@@ -1,9 +1,64 @@
 import expect from 'expect.js';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-addons-test-utils';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import Badge from '../src';
 
-describe('Badge', () => {
+Enzyme.configure({ adapter: new Adapter() });
 
+describe('Uxcore Badge', () => {
+    it('should be able to render count with 1 digit', () => {
+        const wrapper = mount(<Badge count={3} />);
+        const values = wrapper.find('.kuma-badge-count .current');
+        const mergedValue = values.map(el => el.text()).join('');
+
+        expect(mergedValue).to.be('3');
+    });
+
+    it('should be able to render count with 2 digits', () => {
+        const wrapper = mount(<Badge count={23} />);
+        const values = wrapper.find('.kuma-badge-count .current');
+        const mergedValue = values.map(el => el.text()).join('');
+
+        expect(mergedValue).to.be('23');
+    });
+
+    it('should be able to render dot', () => {
+        const wrapper = mount(<Badge dot={true} />);
+        const dot = wrapper.find('sup.kuma-badge-dot');
+
+        expect(dot).to.have.length(1);
+    });
+
+    it('should be able to render text', () => {
+        const wrapper = mount(<Badge text={'test123'} />);
+        const text = wrapper.find('sup.kuma-badge-count').text();
+
+        expect(text).to.be('test123');
+    });
+
+    it('should be able to change count', () => {
+        const wrapper = mount(<Badge count={23} />);
+        wrapper.setProps({ count: 45 });
+
+        const values = wrapper.find('.kuma-badge-count .current');
+        const mergedValue = values.map(el => el.text()).join('');
+
+        setTimeout(() => {
+            expect(mergedValue).to.be('45');
+        }, 10);
+    });
+
+    it('should be able to change count without animation', () => {
+        const wrapper = mount(<Badge count={23} />);
+        wrapper.setState({ animationEnabled: false });
+        wrapper.setProps({ count: 45 });
+
+        const values = wrapper.find('.kuma-badge-count .current');
+        const mergedValue = values.map(el => el.text()).join('');
+
+        setTimeout(() => {
+            expect(mergedValue).to.be('45');
+        }, 10);
+    });
 });
